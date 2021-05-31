b072ebf3e68a28b0ba99f39e153e9157af8032ad
jeanna <jeannawang@hotmail.com>
2021-05-31 19:41:05 +0800

    delete useful comments and code, and format code

--- run_analysis.R
+++ run_analysis.R
@@ -26,9 +26,6 @@ df.subject <- rbind(df.subjecttrain, df.subjecttest)
 names(df.ytrain) <- c("activityno")
 names(df.subject) <- c("subjectno")
 
-# combine train data and test sets to one data set
-# df <- cbind(df.subject, df.y, df.x)
-# print(head(df,2))
 
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 loc.meanstd <- grepl("-(mean|std)\\(\\)",df.features[, 2]) 
@@ -37,29 +34,20 @@ VectFeaturesName <- gsub("\\(\\)", "", df.features[loc.meanstd, 2])             
 VectFeaturesName <- gsub("-", "_", VectFeaturesName) # replace dashes with underline
 names(df.xmeanstd) <- VectFeaturesName
 
-# print(loc.meanstd)
-df.meanstd <- df[, loc.meanstd]
-# View(head(df.meanstd, 5))
-
-
 ## 3. Uses descriptive activity names to name the activities in the data set
-df.activitylabels[,2] <- tolower(sub("_", " ", df.activitylabels[,2]))           ##Rename Var lower without underscore
+df.activitylabels[,2] <- tolower(sub("_", " ", df.activitylabels[,2]))  ## Rename Var lower without underscore
 df.y1 = df.y 
-
-# Temp Variable
-df.y1[,1] = df.activitylabels[df.y1[,1],2]    
-
-# write all activities name for each index 
-names(df.y1) <- c("Activity")
+df.y1[,1] = df.activitylabels[df.y1[,1],2]   # Temp Variable  
+names(df.y1) <- c("Activity")  # write all activities name for each index
 
 
 ## 4. Appropriately labels the data set with descriptive variable names. 
-df.tidytable <- cbind(df.subject, df.y1, df.xmeanstd)                               ## Create a Data Table with subject, Data activity name and measures Tided
-write.table(df.tidytable, file = "merged_Tidy_Data.txt")                            ## Create a plain file with Tidy Data
+df.tidytable <- cbind(df.subject, df.y1, df.xmeanstd)   ## Create a Data Table with subject, Data activity name and measures Tided
+write.table(df.tidytable, file = "merged_Tidy_Data.txt")   ## Create a plain file with Tidy Data
 
 
-##5. From the data set in step 4, creates a second, independent tidy data set with
-##   the average of each variable for each activity and each subject.
+## 5. From the data set in step 4, creates a second, independent tidy data set with
+##    the average of each variable for each activity and each subject.
 df.avg <- df.tidytable %>% 
     group_by(subjectno, Activity) %>% 
     summarise_each(funs(mean))
